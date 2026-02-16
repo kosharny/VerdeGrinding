d394de0e1711d7970d3c870341cf16fc131cef67
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-02-16 15:36:18 +0200

    Initial Commit

---
+++ VerdeGrinding/VerdeGrindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerdeGrindingApp.swift
+//  VerdeGrinding
+//
+//  Created by Maksim Kosharny on 16.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct VerdeGrindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
